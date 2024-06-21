a04ab902a4071501e26b30c527afc7c107d406a8
lucaslunaclaraso <lucaslunaclaraso@gmail.com>
2024-06-21 14:47:32 -0300

    update

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,10 +8,8 @@ import mp from './mp.webp';
 import cripto from './crypto.webp'
 import astro from './astro.webp'
 import axios from 'axios'
-
-
+import './home.css'
 // Componente para representar un enlace individual
-
 const LinkItem = ({ parrafo, title, url, imagen, isImage, gif }) => {
 
   const copyToClipboard = () => {
@@ -25,19 +23,19 @@ const LinkItem = ({ parrafo, title, url, imagen, isImage, gif }) => {
   };
   const nuevoClick = async () => {
     const response = await axios.post('https://apidengue.vercel.app/')
-
   }
-  
+
   return (
-    <div className="link-item">
+    <div className="link-item" style={{ textAlign: 'center', margin: '20px 0', backgroundColor: parrafo && 'transparent' }}>
       {isImage ?
-        <a href={url} target="_blank" rel="noopener noreferrer" onClick={() => nuevoClick()} style={{ width: '10%' }}>
-          <img src={gif ? instrucciones : casino} alt style={{ width: gif ? '100%' : '35%' }} />
+        <a href={url} target="_blank" rel="noopener noreferrer" onClick={() => nuevoClick()} style={{ display: 'block', margin: '0 auto' }}>
+          <img src={gif ? instrucciones : casino} alt="" style={{ width: gif ? '100%' : '35%' }} />
         </a>
 
         :
         <>
-          {parrafo ? <p > <span style={{cursor:'pointer'}} onClick={() => copyToClipboard()}> {title} </span> <br /> <span style={{ fontSize: '12px', color: 'grey' }}>+500% de bonus en los primeros 4 depósitos</span> <br /> <span style={{ fontSize: '12px', color: 'grey' }}>+30% de rakeback semanal</span></p> : title}
+          {parrafo ? <div style={{display:'flex', flexDirection:'column'}}>  <p style={{ fontSize: '3rem', fontWeight: 'bold', color: 'white' }}> BONIFICACIÓN </p>
+            <p className='bonificacion' style={{ fontSize:'2.5rem', position:'absolute',color: '#df003f', textShadow: '-2px -3px 0 #fff,4px -3px 0 #fff,-3px 3px 0 #fff,3px 3px 0 #fff,3px 3px 0 #fff', fontWeight:600 }}>+500%</p> </div> : title}
         </>
 
       }
@@ -56,33 +54,24 @@ const LinkTree = ({ links }) => {
   );
 };
 
-
 // Ejemplo de uso del componente LinkTree
 const Home = () => {
-
-
-
   const links = [
+    { title: 'BONIFICACIÓN +500%', url: '', parrafo: true },
     { url: 'https://1wfsi.com/v3/aggressive-casino', isImage: true },
-    { title: 'Código: dengue1win', url: '', parrafo: true },
     { isImage: true, url: '', gif: true },
     // Agrega más enlaces según sea necesario
   ];
 
   return (
-    <div className="app">
-      <img src={'https://yt3.ggpht.com/xfC9kFjSXQfQqJizTG-_HxE-BnuuQbfbWypn-zLDkFCdgd-NlHG-w2BuKTurVRNaorb5E9d8=s600-c-k-c0x00ffffff-no-rj-rp-mo'} style={{ display: 'flex', width: '100px', height: '100px', borderRadius: '50%', margin: '0 auto' }}></img>
+    <div className="app" style={{ backgroundColor: 'black', padding: '20px', borderRadius: '10px' }}>
+      <img src={'https://yt3.ggpht.com/xfC9kFjSXQfQqJizTG-_HxE-BnuuQbfbWypn-zLDkFCdgd-NlHG-w2BuKTurVRNaorb5E9d8=s600-c-k-c0x00ffffff-no-rj-rp-mo'} style={{ display: 'flex', width: '100px', height: '100px', borderRadius: '50%', margin: '0 auto' }} alt="profile"></img>
       <h3 style={{ textAlign: 'center', color: 'white', fontStyle: 'italic' }}>eldenguee</h3>
       <LinkTree links={links} />
-      {/* <div style={{ display: 'flex', alignItems: 'center', gap: '20px', justifyContent: 'center', marginTop: '15PX' }}>
-        <a href="https://www.instagram.com/eldengueee/" style={{ textDecoration: 'none' }}><SlSocialInstagram style={{ color: 'white' }} /></a>
-        <a href="https://www.youtube.com/@dengueetimba" style={{ textDecoration: 'none' }}> <TiSocialYoutube style={{ color: 'white' }} /></a>
-        <a href="https://www.twitch.tv/eldenguee" style={{ textDecoration: 'none' }}><BsTwitch style={{ color: 'white' }} /></a>
-      </div> */}
-      <div style={{ display: 'flex', alignItems: 'center', gap: '20px', justifyContent: 'center', marginTop: '15PX' }}>
-        <img src={mp} style={{ width: '20%' }} />
-        <img src={cripto} style={{ width: '20%' }} />
-        <img src={astro} style={{ width: '20%' }} />
+      <div style={{ display: 'flex', alignItems: 'center', gap: '20px', justifyContent: 'center', marginTop: '15px' }}>
+        <img src={mp} style={{ width: '20%' }} alt="mercado pago" />
+        <img src={cripto} style={{ width: '20%' }} alt="crypto" />
+        <img src={astro} style={{ width: '20%' }} alt="astropay" />
       </div>
       <h4 style={{ color: 'white', textAlign: 'center' }}>Depósito Mínimo AR$2000</h4>
     </div>
