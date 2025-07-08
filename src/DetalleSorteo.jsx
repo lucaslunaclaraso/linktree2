d8da8d4245ed23040fd8b8d765ea207165d79c96
lucaslunaclaraso <lucaslunaclaraso@gmail.com>
2025-07-08 00:00:07 -0300

    ip socket

--- src/DetalleSorteo.jsx
+++ src/DetalleSorteo.jsx
@@ -251,8 +251,8 @@ export default function DetalleSorteo({ sorteos, setSorteos, isMobile }) {
     if (messageCount >= 200) return { color: '#ff0000cc', bonus: 10 }; // Rojo (+10%)
     if (messageCount >= 150) return { color: '#ed6e07cc', bonus: 8 }; // Naranja (+8%)
     if (messageCount >= 100) return { color: '#bec800cc', bonus: 6 }; // Amarillo (+6%)
-    if (messageCount >= 70) return { color: '#33e64bcc', bonus: 4 }; // Verde Claro (+4%)
-    if (messageCount >= 30) return { color: '#008a13cc', bonus: 2 }; // Verde Oscuro (+2%)
+    if (messageCount >= 40) return { color: '#33e64bcc', bonus: 4 }; // Verde Claro (+4%)
+    if (messageCount >= 10) return { color: '#008a13cc', bonus: 2 }; // Verde Oscuro (+2%)
 
     return { color: 'black', bonus: 0 }; // Default (sin bonus)
   };
